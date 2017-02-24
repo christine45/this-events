6c04b1553c4995b2caa6156b7fa5b8958496649b
Christine <Christine@Christines-MacBook-Pro.local>
2017-02-23 21:44:42 -1000

    THIS event Exercise

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 //Secret Menu Challenge
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
+var RestaurantList = document.getElementsByClassName("name");
+for (var i = 0; i<RestaurantList.length; i++){
+	RestaurantList[i].addEventListener("click", showMenu);
 
+}
+
+function showMenu(){
+	var secretMenu = this.querySelectorAll(".menu")[0];
+		if(secretMenu.style.display === "none"){
+			secretMenu.style.display = "block";
+		}else{
+			secretMenu.style.display = "none";
+		}
+}
